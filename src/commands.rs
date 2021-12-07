c53572a650f02db1237c78f8c92e935e1d316f6a
Jordan Majd <jordan@madtitanstudios.com>
2021-12-06 21:00:31 -0700

    List out potential commands to implement

---
+++ src/commands.rs
@@ -0,0 +1,35 @@
+
+// find devices, list as name:uuid
+fn find_devices() {
+
+}
+
+// list saved devices name:uuid
+fn list_devices() {
+
+}
+
+// add device to saved device, either name or UUID
+fn add_device(name_uuid: String) {
+
+}
+
+// remove a device from saved devices either name or UUID
+fn remove_device(name_uuid: String) {
+
+}
+
+// begin recording a ride, optionally provide a name
+fn start_ride(name: Option<String>) {
+
+}
+
+// list rides 
+fn list_ride() {
+
+}
+
+// export ride to JSON, name or id
+fn export_ride(name_id: String) {
+
+}
